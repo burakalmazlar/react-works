836c3f9621ca642315ee41d8bd39dbb55edc7558
burak almazlar <burak.almazlar@epias.com.tr>
2022-01-06 17:37:53 +0300

    layout imrpuuv #patch

--- 09-practice-project/src/App.js
+++ 09-practice-project/src/App.js
@@ -1,7 +1,23 @@
+import { Redirect, Route } from "react-router-dom";
+
 import Layout from "./components/layout/Layout";
+import QuotesPage from "./pages/quotes-page";
+import NewQuotePage from "./pages/new-quote-page";
 
 function App() {
-  return <Layout />;
+  return (
+    <Layout>
+      <Route exact path="/">
+        <Redirect to="/quotes" />
+      </Route>
+      <Route path="/quotes">
+        <QuotesPage />
+      </Route>
+      <Route path="/new-quote">
+        <NewQuotePage />
+      </Route>
+    </Layout>
+  );
 }
 
 export default App;
--- 09-practice-project/src/components/layout/Layout.js
+++ 09-practice-project/src/components/layout/Layout.js
@@ -1,24 +1,11 @@
 import MainNavigation from "./MainNavigation";
 import classes from "./Layout.module.css";
-import QuotesPage from "../../pages/quotes-page";
-import { Redirect, Route } from "react-router-dom";
-import NewQuotePage from "../../pages/new-quote-page";
 
 const Layout = (props) => {
   return (
     <>
       <MainNavigation />
-      <main className={classes.main}>
-        <Route exact path="/">
-          <Redirect to="/quotes" />
-        </Route>
-        <Route path="/quotes">
-          <QuotesPage />
-        </Route>
-        <Route path="/new-quote">
-          <NewQuotePage />
-        </Route>
-      </main>
+      <main className={classes.main}>{props.children}</main>
     </>
   );
 };
